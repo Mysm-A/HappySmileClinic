25690477cae9ddcdb413a6986c2786e085c0e797
Mysm-A <159068299+Mysm-A@users.noreply.github.com>
2025-09-26 21:29:26 +0300

    Update Web.js

--- src/Web.js
+++ src/Web.js
@@ -1,5 +1,5 @@
 import React ,{useEffect} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from  './components/login/Login';
 import SignUp from './components/signup/Signup';
  import Navbar from  './components/Header/Navbar';
